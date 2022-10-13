03cb2e367961f300250e7d4fd7bb0ba0b8642eb1
Runrong Liu <lrrong2020@gmail.com>
2022-10-14 01:16:55 +0800

    suit

--- testColor.py
+++ testColor.py
@@ -6,19 +6,25 @@ rightPos = (1519, 1090)
 spadeRGB = (0, 0, 0)
 heartRGB = (202,44,43)
 clubRGB = (23, 183, 23)
-diamondRGB = 66, 64, 207)
+diamondRGB = (66, 64, 207)
 
 
-def belongsColor(*args):   
-    if pyautogui.pixelMatchesColor(*args, spadeRGB, tolerance=50):
+def belongsColor(pos):   
+    if pyautogui.pixelMatchesColor(*pos, (0, 0, 0), tolerance=50):
         return 's'
-    elif pyautogui.pixelMatchesColor(*args, heartRGB, tolerance=50):
+    elif pyautogui.pixelMatchesColor(*pos, (202,44,43), tolerance=50):
         return 'h'
-    elif pyautogui.pixelMatchesColor(*args, clubRGB, tolerance=50):
+    elif pyautogui.pixelMatchesColor(*pos, (23, 183, 23), tolerance=50):
         return 'c'
-    elif pyautogui.pixelMatchesColor(*args, diamondRGB, tolerance=50):
+    elif pyautogui.pixelMatchesColor(*pos, (66, 64, 207), tolerance=50):
         return 'd'
     else:
         return 'x'
 
 print('Left: '+belongsColor(leftPos))
+print('Right: '+belongsColor(rightPos))
+
+'''
+b = pyautogui.pixelMatchesColor(1519, 1090, (23, 183, 23), tolerance=50)
+print(b)
+'''
