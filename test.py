aa85c16fe5e021faedc567a679ed3ad585b7f441
Runrong Liu <lrrong2020@gmail.com>
2022-10-14 01:39:23 +0800

    Hand recognition

--- test.py
+++ test.py
@@ -1,54 +1,62 @@
 import pyautogui
 
-myArea = (1350, 998, 200, 70)
-
-img = pyautogui.screenshot(region=myArea)
-
-Cards = ['2L',
-         '3L',
-         '4L',
-         '5L',
-         '6L',
-         '7L',
-         '8L',
-         '9L',
-         'TL',
-         'JL',
-         'QL',
-         'KL',
-         'AL',
-         '2R',
-         '3R',
-         '4R',
-         '5R',
-         '6R',
-         '7R',
-         '8R',
-         '9R',
-         'TR',
-         'JR',
-         'QR',
-         'KR',
-         'AR',
-    ]
 
 myHand=[]
 
-for i in Cards:
-    fileName = str('Numbers\\' + i + '.jpg')
-    #print('searching for: ' + fileName)
-    result = pyautogui.locateAll(fileName, img, confidence=0.98, grayscale=True)
+def getNumber():
+    myArea = (1350, 998, 200, 70)
+
+    img = pyautogui.screenshot(region=myArea)
+
+    Cards = ['2L','3L','4L','5L','6L','7L','8L','9L','TL','JL','QL','KL','AL','2R','3R','4R','5R','6R','7R','8R', '9R','TR','JR','QR','KR','AR',]
+
+    for i in Cards:
+        fileName = str('Numbers\\' + i + '.jpg')
+        #print('searching for: ' + fileName)
+        result = pyautogui.locateAll(fileName, img, confidence=0.98, grayscale=True)
     
-    resultList = list(result)
-    #print(resultList)
-    if not resultList:
-        continue
-    elif len(resultList) == 1:
-        myHand.append(i[:1])
-    else:
-        raise Exception("Confidence too low")
+        resultList = list(result)
+        #print(resultList)
+        if not resultList:
+            continue
+        elif len(resultList) == 1:
+            myHand.append(i[:1])
+        else:
+            raise Exception("Confidence too low")
 
-if len(myHand) < 2:
-    raise Exception("Confidence too high")
+    if len(myHand) < 2:
+        raise Exception("Confidence too high")
 
+    #print(list(myHand))
+
+def getSuit():
+    leftPos = (1432, 1089)
+    rightPos = (1519, 1090)
+
+    spadeRGB = (0, 0, 0)
+    heartRGB = (202,44,43)
+    clubRGB = (23, 183, 23)
+    diamondRGB = (66, 64, 207)
+
+
+    def belongsColor(pos):   
+        if pyautogui.pixelMatchesColor(*pos, (0, 0, 0), tolerance=50):
+            return 's'
+        elif pyautogui.pixelMatchesColor(*pos, (202,44,43), tolerance=50):
+            return 'h'
+        elif pyautogui.pixelMatchesColor(*pos, (23, 183, 23), tolerance=50):
+            return 'c'
+        elif pyautogui.pixelMatchesColor(*pos, (66, 64, 207), tolerance=50):
+            return 'd'
+        else:
+            return 'x'
+
+    #print('Left: '+belongsColor(leftPos))
+    #print('Right: '+belongsColor(rightPos))
+
+    myHand[0]+=belongsColor(leftPos)
+    myHand[1]+=belongsColor(rightPos)
+
+getNumber()
+getSuit()
 print(list(myHand))
