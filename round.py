83d617605a7f9b13214fb27444464b7355360751
Runrong Liu <lrrong2020@gmail.com>
2022-10-14 11:01:08 +0800

    my turn

--- round.py
+++ round.py
@@ -1,14 +1,27 @@
 import threading
+import pyautogui
+
+whiteBlockPoint2k = (2225, 1111)
+whiteBlockPoint1080 = (1677, 834)
+yellowCirclePoint1080 = (1655, 794)
 
 thread=None
+isMyTurn=False
 
 def getMyRound():
   thread=threading.Timer(0.5, getMyRound)
   thread.start()
-  print( "Hello, World!")
-
-def isWhite():
+  isMyTurn=isYellowCircleDisplaying()
+  if isMyTurn:
+      print("My Turn!")
+      thread.cancel()
+  else:
+      print("Not my turn")
   
+
+def isYellowCircleDisplaying():
+  return not pyautogui.pixelMatchesColor(*yellowCirclePoint1080, (0, 0, 0), tolerance=50)
+
 
 getMyRound()
 
