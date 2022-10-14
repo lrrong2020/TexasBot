e3cebb8339b7b618b96ace84f3f3366de9b00000
Runrong Liu <lrrong2020@gmail.com>
2022-10-14 12:12:08 +0800

    auto all-in

--- round.py
+++ round.py
@@ -1,30 +1,36 @@
 import threading
 import pyautogui
+import time
 
 whiteBlockPoint2k = (2225, 1111)
 whiteBlockPoint1080 = (1677, 834)
 yellowCirclePoint1080 = (1655, 794)
 
+
 thread=None
 isMyTurn=False
 
 def getMyRound():
-  thread=threading.Timer(0.5, getMyRound)
+  thread=threading.Timer(0.6, getMyRound)
   thread.start()
-  isMyTurn=isYellowCircleDisplaying()
+
+  isYellowCircleDisplaying(thread)
+
+def isYellowCircleDisplaying(thread):
+  isMyTurn = not pyautogui.pixelMatchesColor(*yellowCirclePoint1080, (0, 0, 0), tolerance=50)
   if isMyTurn:
       print("My Turn!")
       
-      pyautogui.press('f')
+      #pyautogui.press('f')
       
+      pyautogui.press('r')
+      pyautogui.click(x=1400, y=918)#all-in
+      pyautogui.click(x=1724, y=958)#enter
+      
+      isMyTurn=False
       thread.cancel()
   else:
       print("Not my turn")
-  
-
-def isYellowCircleDisplaying():
-  return not pyautogui.pixelMatchesColor(*yellowCirclePoint1080, (0, 0, 0), tolerance=50)
-
 
 getMyRound()
 
